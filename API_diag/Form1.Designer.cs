eb253cb595cc5ad01e9ab8e3f8a969ad3772872a
Nicolas Champy <nicolachampy@gmail.com>
2026-07-23 13:27:25 +0200

    Initial commit

---
+++ API_diag/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace API_diag
+{
+    partial class WinMoStore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinMoStore));
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItem1);
+            this.mainMenu1.MenuItems.Add(this.menuItem2);
+            // 
+            // menuItem1
+            // 
+            resources.ApplyResources(this.menuItem1, "menuItem1");
+            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            // 
+            // menuItem2
+            // 
+            resources.ApplyResources(this.menuItem2, "menuItem2");
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // WinMoStore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Menu = this.mainMenu1;
+            this.Name = "WinMoStore";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuItem menuItem2;
+    }
+}
+
---
+++ API_diag/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using CodeBetter.Json;
+
+namespace API_diag
+{
+    public partial class WinMoStore : Form
+    {
+        public WinMoStore()
+        {
+            InitializeComponent();
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+            string apiUrl = "http://192.168.1.116:3000/api/applications";
+            string jsonResponse = GetApiData(apiUrl);
+
+            if (jsonResponse.StartsWith("Erreur"))
+            {
+                label1.Text = jsonResponse;
+                return;
+            }
+
+            try
+            {
+                ApiResponse apiResponse = Converter.Deserialize<ApiResponse>(jsonResponse);
+
+                if (!apiResponse.success)
+                {
+                    label1.Text = "Échec API : " + apiResponse.message;
+                    return;
+                }
+
+                AfficherApplications(apiResponse.data);
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Erreur de parsing JSON : " + ex.Message;
+            }
+        }
+
+        // Crée un panel + label pour chaque application, empilés verticalement dans panel1
+        private void AfficherApplications(ApplicationApi[] apps)
+        {
+            panel1.SuspendLayout();
+
+            // Vide le contenu précédent (utile si on rafraîchit la liste plusieurs fois)
+            panel1.Controls.Clear();
+
+            const int hauteurItem = 40;   // hauteur de chaque sous-panel
+            const int marge = 2;          // espace vertical entre chaque item
+
+            for (int i = 0; i < apps.Length; i++)
+            {
+                ApplicationApi app = apps[i];
+
+                Panel itemPanel = new Panel();
+                itemPanel.Name = "panelApp" + i;
+                itemPanel.Left = 0;
+                itemPanel.Top = i * (hauteurItem + marge);
+                itemPanel.Width = panel1.ClientSize.Width;
+                itemPanel.Height = hauteurItem;
+                itemPanel.BackColor = Color.Blue;
+
+                Label lbl = new Label();
+                lbl.Name = "labelApp" + i;
+                lbl.Text = app.name;
+                lbl.Left = 5;
+                lbl.Top = 5;
+                lbl.Width = itemPanel.Width - 10;
+                lbl.Height = hauteurItem - 10;
+                lbl.ForeColor = Color.White;
+
+                itemPanel.Controls.Add(lbl);
+                panel1.Controls.Add(itemPanel);
+            }
+
+            panel1.ResumeLayout();
+        }
+
+        private string GetApiData(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string result = reader.ReadToEnd();
+                        return result;
+                    }
+                }
+            }
+            catch (WebException webEx)
+            {
+                return "Erreur : " + webEx.Message;
+            }
+        }
+
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    public class ApplicationApi
+    {
+        public string id;
+        public string name;
+    }
+
+    public class ApiResponse
+    {
+        public bool success;
+        public string message;
+        public ApplicationApi[] data;
+    }
+}
