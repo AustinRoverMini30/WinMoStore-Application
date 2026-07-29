51761d0398ff7624a4d15ee270e35a542f89d215
Nicolas Champy <nicolachampy@gmail.com>
2026-07-29 23:51:04 +0200

    Adding loading cursor.

--- API_diag/Form1.cs
+++ API_diag/Form1.cs
@@ -14,32 +14,9 @@ using CodeBetter.Json;
 
 namespace API_diag
 {
-    public static class Theme
-    {
-        public static readonly Color CouleurFond = Color.FromArgb(232, 232, 232);
-        public static readonly Color CouleurCarte = Color.White;
-        public static readonly Color CouleurCartePressee = Color.FromArgb(225, 238, 250);
-        public static readonly Color CouleurBordure = Color.FromArgb(205, 205, 205);
-        public static readonly Color CouleurAccent = Color.FromArgb(0, 122, 204);
-        public static readonly Color CouleurAccentFonce = Color.FromArgb(0, 92, 160);
-        public static readonly Color CouleurTexte = Color.FromArgb(40, 40, 40);
-        public static readonly Color CouleurTexteSecondaire = Color.FromArgb(130, 130, 130);
-        public static readonly Color CouleurTexteClair = Color.White;
-        public static readonly Color CouleurSanteInconnue = Color.FromArgb(160, 160, 160);
-        public static readonly Color CouleurSanteOk = Color.FromArgb(76, 175, 80);
-        public static readonly Color CouleurSanteKo = Color.FromArgb(220, 53, 69);
-
-        public static readonly Font PoliceEntete = new Font("Tahoma", 10, FontStyle.Bold);
-        public static readonly Font PoliceTitreDetail = new Font("Tahoma", 13, FontStyle.Bold);
-        public static readonly Font PoliceNormale = new Font("Tahoma", 10, FontStyle.Regular);
-        public static readonly Font PoliceNormaleGrasse = new Font("Tahoma", 10, FontStyle.Bold);
-        public static readonly Font PolicePetite = new Font("Tahoma", 8, FontStyle.Regular);
-        public static readonly Font PoliceBouton = new Font("Tahoma", 10, FontStyle.Bold);
-    }
-
     public partial class WinMoStore : Form
     {
-        private const string ApiBaseUrl = "http://192.168.1.33:3000";
+        private const string ApiBaseUrl = "http://192.168.1.102:3000";
 
         // --- Page liste ---
         private Panel panelHeaderListe;
@@ -441,6 +418,9 @@ namespace API_diag
             label1.ForeColor = Theme.CouleurTexteSecondaire;
             panel1.Controls.Clear();
 
+            Cursor.Current = Cursors.WaitCursor;
+            Cursor.Show();
+
             Thread threadChargement = new Thread(new ThreadStart(delegate
             {
                 string jsonResponse = null;
@@ -464,6 +444,9 @@ namespace API_diag
         private void ChargementTermine(string jsonResponse, string erreur)
         {
             chargementEnCours = false;
+
+            Cursor.Current = Cursors.Default;
+            Cursor.Hide();
 
             if (erreur != null)
             {
---
+++ API_diag/Theme.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace API_diag
+{
+    public static class Theme
+    {
+        public static readonly Color CouleurFond = Color.FromArgb(232, 232, 232);
+        public static readonly Color CouleurCarte = Color.White;
+        public static readonly Color CouleurCartePressee = Color.FromArgb(225, 238, 250);
+        public static readonly Color CouleurBordure = Color.FromArgb(205, 205, 205);
+        public static readonly Color CouleurAccent = Color.FromArgb(0, 122, 204);
+        public static readonly Color CouleurAccentFonce = Color.FromArgb(0, 92, 160);
+        public static readonly Color CouleurTexte = Color.FromArgb(40, 40, 40);
+        public static readonly Color CouleurTexteSecondaire = Color.FromArgb(130, 130, 130);
+        public static readonly Color CouleurTexteClair = Color.White;
+        public static readonly Color CouleurSanteInconnue = Color.FromArgb(160, 160, 160);
+        public static readonly Color CouleurSanteOk = Color.FromArgb(76, 175, 80);
+        public static readonly Color CouleurSanteKo = Color.FromArgb(220, 53, 69);
+
+        public static readonly Font PoliceEntete = new Font("Tahoma", 10, FontStyle.Bold);
+        public static readonly Font PoliceTitreDetail = new Font("Tahoma", 13, FontStyle.Bold);
+        public static readonly Font PoliceNormale = new Font("Tahoma", 10, FontStyle.Regular);
+        public static readonly Font PoliceNormaleGrasse = new Font("Tahoma", 10, FontStyle.Bold);
+        public static readonly Font PolicePetite = new Font("Tahoma", 8, FontStyle.Regular);
+        public static readonly Font PoliceBouton = new Font("Tahoma", 10, FontStyle.Bold);
+    }
+}
